bce9133a6bb8b14cf9f63dc6bab0c00ad4be8ee3
huangs <huangs@chalmers.se>
2025-02-18 22:17:39 +0000

    Add simple finding and avoidance logic

--- src/opendlv-uav-control-lowlevel.cpp
+++ src/opendlv-uav-control-lowlevel.cpp
@@ -27,16 +27,56 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
- 
-template <size_t N>
-std::string arrayToString(const std::array<uint8_t, N>& arr) {
-    std::ostringstream oss;
-    for (size_t i = 0; i < N; ++i) {
-        oss << static_cast<uint8_t>(arr[i]) << " ";  // 轉為 int 避免 char 解析問題
-    }
-    return oss.str(); // 會有尾部空格
+#include <cmath>
+
+
+void Takeoff(cluon::OD4Session &od4, float height, float duration){
+    std::cout << "Taking off to height: " << height << std::endl;        
+    cluon::data::TimeStamp sampleTime;
+    opendlv::logic::action::CrazyFlieCommand cfcommand;
+    cfcommand.height(height);
+    cfcommand.time(duration);
+    od4.send(cfcommand, sampleTime, 0);
 }
 
+void Goto(cluon::OD4Session &od4, float x, float y, float z, float yaw, float duration){
+    std::cout << "Go to position : x: "<< x << " ,y: " << y << " ,z: " << z << " , yaw: " << yaw << std::endl;
+    cluon::data::TimeStamp sampleTime;
+    opendlv::logic::action::CrazyFlieCommand cfcommand;
+    cfcommand.x(x);
+    cfcommand.y(y);
+    cfcommand.z(z);
+    cfcommand.yaw(yaw);
+    cfcommand.time(duration);
+    od4.send(cfcommand, sampleTime, 3);
+}
+
+void Landing(cluon::OD4Session &od4, float height, float duration){
+    std::cout << "Landing to height: " << height << std::endl;
+    cluon::data::TimeStamp sampleTime;
+    opendlv::logic::action::CrazyFlieCommand cfcommand;
+    cfcommand.height(height);
+    cfcommand.time(duration);
+    od4.send(cfcommand, sampleTime, 1);
+}
+
+void Stopping(cluon::OD4Session &od4){
+    std::cout << "Stopping." << std::endl;
+    cluon::data::TimeStamp sampleTime;
+    opendlv::logic::action::CrazyFlieCommand cfcommand;
+    od4.send(cfcommand, sampleTime, 2);
+}
+
+/*
+Input: 
+- front/rear/left/right IR sensor distance
+- purple ball position(If some exist, to dodge it)
+- green ball position(For goal)
+- charging base position
+
+Output:
+- Corresponding actions
+*/
 int32_t main(int32_t argc, char **argv) {
     int32_t retCode{1};
     auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
@@ -48,83 +88,130 @@ int32_t main(int32_t argc, char **argv) {
     // Interface to a running OpenDaVINCI session; here, you can send and receive messages.
     cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
 
-    // // Handler to receive distance readings (realized as C++ lambda).
-    // std::mutex distancesMutex;
-    // float front{0};
-    // float rear{0};
-    // float left{0};
-    // float right{0};
-    // auto onDistance = [&distancesMutex, &front, &rear, &left, &right](cluon::data::Envelope &&env){
-    //     auto senderStamp = env.senderStamp();
-    //     // Now, we unpack the cluon::data::Envelope to get the desired DistanceReading.
-    //     opendlv::proxy::DistanceReading dr = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(env));
-    //     // Store distance readings.
-    //     std::lock_guard<std::mutex> lck(distancesMutex);
-    //     switch (senderStamp) {
-    //         case 0: front = dr.distance(); break;
-    //         case 2: rear = dr.distance(); break;
-    //         case 1: left = dr.distance(); break;
-    //         case 3: right = dr.distance(); break;
-    //     }
-    // };
-    // // Finally, we register our lambda for the message identifier for opendlv::proxy::DistanceReading.
-    // od4.dataTrigger(opendlv::proxy::DistanceReading::ID(), onDistance);
+    // Handler to receive distance readings (realized as C++ lambda).
+    std::mutex distancesMutex;
+    float front{0};
+    float rear{0};
+    float left{0};
+    float right{0};
+    auto onDistance = [&distancesMutex, &front, &rear, &left, &right](cluon::data::Envelope &&env){
+        auto senderStamp = env.senderStamp();
+        // Now, we unpack the cluon::data::Envelope to get the desired DistanceReading.
+        opendlv::proxy::DistanceReading dr = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(env));
+        // Store distance readings.
+        std::lock_guard<std::mutex> lck(distancesMutex);
+        switch (senderStamp) {
+            case 0: front = dr.distance(); break;
+            case 1: rear = dr.distance(); break;
+            case 2: left = dr.distance(); break;
+            case 3: right = dr.distance(); break;
+        }
+    };
+    // Finally, we register our lambda for the message identifier for opendlv::proxy::DistanceReading.
+    od4.dataTrigger(opendlv::proxy::DistanceReading::ID(), onDistance);
 
     // Endless loop; end the program by pressing Ctrl-C.
-    if (od4.isRunning()) {
-        ////////////////////////////////////////////////////////////////
-        // Example for creating and sending a message to other microservices; can
-        // be removed when not needed.
-        // Requires high level commander enabled (param commander.enHighLevel == 1)
-        std::cout << "Taking off..." << std::endl;        
-        cluon::data::TimeStamp sampleTime;
-        opendlv::logic::action::CrazyFlieCommand cfcommand;
-        cfcommand.height(0.5f);
-        cfcommand.yaw(0.0f);
-        cfcommand.time(3.0f);
-        // auto takeoffArray = PacketUtils::takeoffCommand(0.5f, 0.0f, 3.0f);   
-        // std::string strCommand = arrayToString(takeoffArray);
-        // // std::string strCommand;
-        // // strCommand.assign(takeoffArray.begin(), takeoffArray.end());          
-        // uint32_t takeoffSize = takeoffArray.size();  
-        // std::cout << "Command to send: " << strCommand << ", size: " << takeoffSize << std::endl;   
-        // cfcommand.size(takeoffSize);
-        // cfcommand.command(strCommand);
-        od4.send(cfcommand, sampleTime, 0);
-        // cf.getCon().send(PacketUtils::takeoffCommand(0.5f, 0.0f, 3.0f));
-        std::this_thread::sleep_for(std::chrono::milliseconds(3250));
+    float safe_dist = 0.5f;
+    float front_looking_dist = 0.5f;
+    float dist_moved = 0.0f;
+    float dist_moved_so_far = 0.0f;
+    float angle_moved = 0.0f;
+    int Timer = 0;
+    int timer_max = 1000;
+    bool Turning_mode = false;
+    bool hasTakeoff = false;
+    while (od4.isRunning()) {
+        // Sleep for 100 ms to not let the loop run to fast
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        std::cout << "Go to..." << std::endl;
-        // auto gotoArray = PacketUtils::gotoCommand(0.5f, 0.5f, 1.0f, 0.0f, 3.0f);
-        // strCommand = arrayToString(gotoArray);
-        // uint32_t gotoSize = gotoArray.size();
-        cfcommand.x(0.5f);
-        cfcommand.y(0.5f);
-        cfcommand.z(1.0f);
-        cfcommand.yaw(0.0f);
-        cfcommand.time(3.0f);
-        od4.send(cfcommand, sampleTime, 3);
-        // cf.getCon().send(PacketUtils::gotoCommand(0.5f, 0.5f, 1.0f, 0.0f, 3.0f));
-        std::this_thread::sleep_for(std::chrono::milliseconds(3250));
+        // Takeoff first
+        if ( hasTakeoff == false ){
+            Takeoff(od4, 0.5f, 1.0f);
+            hasTakeoff = true;
+        }
 
-        std::cout << "Landing..." << std::endl;
-        // auto landArray = PacketUtils::landCommand(0.0f, 0.0f, 3.0f);
-        // strCommand = arrayToString(landArray);
-        cfcommand.height(0.0f);
-        cfcommand.yaw(0.0f);
-        cfcommand.time(3.0f);
-        od4.send(cfcommand, sampleTime, 0);
-        // cf.getCon().send(PacketUtils::landCommand(0.0f, 0.0f, 3.0f));
-        std::this_thread::sleep_for(std::chrono::milliseconds(3250));
+        /*
+            Obstacle avoidance
+        */
+        // Check that whether the crazyflie is too close to something
+        // If left and right less than 0.5 m
+        if ( left <= safe_dist || right <= safe_dist ){
+            // Move away from wall until left and right larger than 0.5
+            float dist = 0.1f;
+            if ( left <= safe_dist ){
+                // Move right
+                Goto(od4, 0.0f, -dist, 0.0f, 0.0f, 3.0f);
+            }
+            else if ( right <= safe_dist ){
+                // Move left
+                Goto(od4, 0.0f, dist, 0.0f, 0.0f, 3.0f);
+            }
+            continue;
+        }
 
-        std::cout << "Stopping..." << std::endl;
-        // auto stopArray = PacketUtils::stopCommand();
-        od4.send(cfcommand, sampleTime, 2);
-        // cf.getCon().send(PacketUtils::stopCommand());
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        // Check that whether there are purple balls occur in the vision
+        /*
+            Homing
+        */
+
+        /*
+            Target reaching
+        */
+        // Reset front looking dist while some target occur
+
+        
+        /*
+            Finding
+        */
+       // If the front is larger than the safe dist and the turning mode is off, move to the destination
+       if ( front > safe_dist && Turning_mode == false ){
+            float dist_to_move = 0.1f;
+            Goto(od4, dist_to_move, 0.0f, 0.0f, 0.0f, 1.0f);
+            dist_moved += dist_to_move;
+            Timer += 1;
+            continue;
+        }           
+
+        // Turn the turning mode on
+        if ( Turning_mode == false ){
+            Turning_mode = true;
+        }
+        
+        // Record dist moved
+        if ( dist_moved != 0.0f && dist_moved_so_far < dist_moved ){
+            dist_moved_so_far = dist_moved;
+            dist_moved = 0;
+        }     
+
+        // Change the front looking distance while the dist moved got stucked
+        if ( Timer >= timer_max && dist_moved_so_far <= front_looking_dist ){
+            front_looking_dist += 0.5f;
+            dist_moved_so_far = 0;
+            Timer = 0;
+        }
+
+        // If the crazyflie has already turned around and can not find anything, shorten the front looking dist
+        if ( angle_moved >= 2 * M_PI ){
+            front_looking_dist -= 0.2f;
+            angle_moved = 0;
+        }
+        
+        // Tuning around to find somewhere to go to 
+        if ( front <= front_looking_dist ){
+            // Turn around to see whether somewhere can go
+            float angle = 5.0f / 180.0f * M_PI;
+            Goto(od4, 0.0f, 0.0f, 0.0f, angle, 1.0f);
+            angle_moved += angle;
+        }
+        else{
+            // Turn off turning mode while some directions found
+            Turning_mode = false;
+
+            // Reset moved angle
+            angle_moved = 0;
+        }
     }
 
     retCode = 0;
     return retCode;
 }
-
